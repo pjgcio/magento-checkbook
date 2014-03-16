09ab2f58984cf8e0f757128c61225e2ab947af5a
Utkarsh Sengar <utkarsh2012@gmail.com>
2014-03-16 00:10:31 -0700

    initial commit

---
+++ app/code/local/Checkbook/Block/Form/Checkbook.php
@@ -0,0 +1,9 @@
+<?php
+class Checkbook_Block_Form_Pay extends Mage_Payment_Block_Form
+{
+	protected function _construct()
+	{
+		parent::_construct();
+		$this->setTemplate('checkbook/form/checkbook.phtml');
+	}
+}
---
+++ app/code/local/Checkbook/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class Checkbook_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
