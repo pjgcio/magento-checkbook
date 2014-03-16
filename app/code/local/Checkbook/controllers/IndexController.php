4956c9f947522ceb9150bddc231d969bdd57f6d1
Utkarsh Sengar <utkarsh2012@gmail.com>
2014-03-16 00:52:18 -0700

    added new endpoint to magento

---
+++ app/code/local/Checkbook/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+class Checkbook_IndexController extends Mage_Core_Controller_Front_Action
+{
+   public function indexAction ()
+   {
+     echo 'test index';
+   }
+   public function mamethodeAction ()
+   {
+     echo 'test mymethod';
+    }
+}
